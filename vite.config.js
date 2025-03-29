d1917ad05f4e6323e0c55c0da723cb659c44210e
Andres Velez <andresjobpk@gmail.com>
2025-03-29 06:48:05 -0500

    Cambiando direccion del vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/andrias01/andrias01.github.io.git",
 })
