9bdccf0061fe740854a18b023a0d999b81290bda
Andres Velez <andresjobpk@gmail.com>
2025-03-29 06:22:46 -0500

    FrontEnd Proyecto Victus

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+//import Home from './components/Home'
+import NavBurguer from './components/NavBurguer'
+
+function App() {
+  
+
+  return (
+    //<Home></Home>
+    <NavBurguer></NavBurguer>
+  )
+}
+
+export default App
