abda908f3001cc67acdf2402cc4e57f2e260ce64
Andres Velez <andresjobpk@gmail.com>
2025-03-29 06:57:23 -0500

    na

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/andrias01/andrias01.github.io.git",
+  base: "https://andrias01.github.io/andrias01.github.io",
 })
